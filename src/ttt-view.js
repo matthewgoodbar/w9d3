317b74b7fb7586c16f94cdce02e4ca29b8434a13
Patrick Choi <ptrckchoi@gmail.com>
2022-11-30 15:19:43 -0800

    debugging click events

--- src/ttt-view.js
+++ src/ttt-view.js
@@ -3,9 +3,12 @@ class View {
     this.game = game;
     this.el = el;
     this.setupBoard();
+    this.bindEvents();
   }
 
   setupBoard() {
+    console.log('setBoard');
+
     let board = document.createElement("ul");
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -17,12 +20,26 @@ class View {
     this.el.appendChild(board);
   }
   
-  bindEvents() {}
+  bindEvents() {
+    this.el.addEventListener('click', handleClick); //gives our grid this click event
+    console.log('bindEvents running');
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    let square = e.target;
+    console.log('HELLO')
+    if (square.nodeName === 'li'){
+      this.game.playMove(square.getAttribute('data-pos'));
+      this.makeMove(square);
+    }
 
-  makeMove(square) {}
+  }
 
+  makeMove(square) {
+    square.innerHTML = 'X';
+    square.style.backgroundColor = "white";
+  }
+ 
 }
 
 module.exports = View;
